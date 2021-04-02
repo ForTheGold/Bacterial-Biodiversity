e1be5a761f692c2db07b58949c82754289b80f53
Ben <benjatubbs@gmail.com>
2021-04-02 14:14:57 -0700

    Adding dynamically updating data

--- plots.js
+++ plots.js
@@ -12,27 +12,27 @@ function init() {
     });
 })}
 
-// d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-// function updatePlotly() {
-//   var dropdownMenu = d3.select("#dropdownMenu");
-//   var dataset = dropdownMenu.property("value");
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-//   var xData = [1, 2, 3, 4, 5];
-//   var yData = [];
+    PANEL.html("");
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("ETHINICITY: " + result.ethnicity);
+    PANEL.append("h6").text("GENDER: " + result.gender);
+    PANEL.append("h6").text("AGE: " + result.age);
+    PANEL.append("h6").text("LOCATION: " + result.location);
+    PANEL.append("h6").text("BBTYPE: " + result.bbtype);
+    PANEL.append("h6").text("WFREQ: " + result.wfreq);
+  });
+}
 
-//   if (dataset === 'dataset1') {
-//     yData = [1, 2, 4, 8, 16];
-//   };
-
-//   if (dataset === 'dataset2') {
-//     yData = [1, 10, 100, 1000, 10000];
-//   };
-
-//   var trace = {
-//     x: [xData],
-//     y: [yData],
-//   };
-//   Plotly.restyle("plot", trace);
-// };
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  //buildCharts(newSample);
+}
 
 init();
